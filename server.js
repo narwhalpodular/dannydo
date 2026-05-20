e6901387b426d47be3802df95b1f162ae342a80a
narwhalpodular <danieljamesmcenerney@gmail.com>
2026-05-20 12:23:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on ${PORT}`));
 
 // Middleware
 app.use(cors());
